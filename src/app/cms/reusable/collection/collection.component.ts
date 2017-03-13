36b4e09c71e2dc6ee2052642ef9333e7c2a88e93
Laura Pigott <pigottlaura@gmail.com>
2017-03-13 22:02:46 +0000

    Adding and deleting of items in an array

--- src/app/cms/reusable/collection/collection.component.ts
+++ src/app/cms/reusable/collection/collection.component.ts
@@ -34,4 +34,37 @@ export class CollectionComponent {
   viewMediaItems(itemEncapsulationPath){
     this.collectionRequestToViewMediaItems.emit(itemEncapsulationPath);
   }
+
+  deleteItem(encapsulationPath, index){
+    if(this.projectContent[encapsulationPath] != null){
+      switch(this.projectContent[encapsulationPath].constructor.name.toLowerCase()){
+        case "array":{
+          this.projectContent[encapsulationPath].splice(index, 1);
+          break;
+        }
+      }
+    }
+  }
+
+  addNewItem(encapsulationPath, contentType){
+    if(this.projectContent[encapsulationPath] != null){
+      if(this.projectStructure[encapsulationPath] != null){
+        if(this.projectStructure[encapsulationPath].items != null){
+          if(this.projectStructure[encapsulationPath].type != null && this.projectStructure[encapsulationPath].type == "array"){
+            var newItem = this._createNewItem(this.projectStructure[encapsulationPath].items);
+            this.projectContent[encapsulationPath].push(newItem);
+            this.collectionContentChanged({path: encapsulationPath, content: this.projectContent[encapsulationPath]});
+          }
+        }
+      }
+    }
+  }
+
+  private _createNewItem(itemsStructure){
+    var newItem = {};
+    for(var attribute in itemsStructure){
+      newItem[attribute] = null;
+    }
+    return newItem;
+  }
 }
